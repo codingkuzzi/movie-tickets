a6f74d7a876518470fc08315385ce23b15633e02
Anna Kuznetsova <aakuznetsova@gmail.com>
2017-06-13 19:39:21 -0700

    change prototype function and add it to the back-end logic in js

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,26 @@ function Ticket(movie, time, age, cost) {
   this.cost = cost;
 }
 
+Ticket.prototype.Cost = function(movieName, time, age) {
+  if(movieName === 'Wonder Woman' || movieName === 'Cars 3') {
+    this.cost +=10;
+  } else {
+    this.cost +=6;
+  }
+  if(time === '9:00 AM - 12:00 PM') {
+    this.cost +=1;
+  } else if(time === '12:00 PM - 15:00 PM') {
+      this.cost +=3;
+    } else {
+      this.cost +=5;
+  }
+  if(age === '60 +') {
+    this.cost -= 5;
+  } else {
+    this.cost += 0;
+  }
+  return this.cost
+}
 
 // user interface logic
 $(document).ready(function() {
@@ -17,27 +37,8 @@ $(document).ready(function() {
     var selectedAge = $("select#age").val();
 
     var newTicket = new Ticket(selectedMovieName, selectedTime, selectedAge, 0)
+    var newCost = newTicket.Cost(selectedMovieName, selectedTime, selectedAge)
 
-    Ticket.prototype.Cost = function() {
-      if(selectedMovieName === 'Wonder Woman' || selectedMovieName === 'Cars 3') {
-        this.cost +=10;
-      } else {
-        this.cost +=6;
-      }
-      if(selectedTime === '9:00 AM - 12:00 PM') {
-        this.cost +=1;
-      } else if(selectedTime === '12:00 PM - 15:00 PM') {
-          this.cost +=3;
-        } else {
-          this.cost +=5;
-      }
-      if(selectedAge === '60 +') {
-        this.cost -= 5;
-      } else {
-        this.cost += 0;
-      }
-      return this.cost
-    }
-    $("#result").append("<h3>" + "You've chosen " + newTicket.movieName + ", " + newTicket.time + ", " + " age " + newTicket.age + "." + "</h3>" + "<h3>" + "The cost of one ticket" + " is $ " + newTicket.Cost() + "." + "</h3>");
+    $("#result").append("<h3>" + "You've chosen " + newTicket.movieName + ", " + newTicket.time + ", " + " age " + newTicket.age + "." + "</h3>" + "<h3>" + "The cost of one ticket" + " is $ " + newCost + "." + "</h3>");
   });
 });
